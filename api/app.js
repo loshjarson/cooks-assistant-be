f37291a964261966c6214dcf99db2b310a7750ca
loshjarson <jlarson129@gmail.com>
2023-05-15 17:50:04 -0400

    middleware

--- api/app.js
+++ api/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const cors = require('cors');
+const logger = require('morgan');
+const helmet = require('helmet'); 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const dotenv = require('dotenv');
+const config_result = dotenv.config();
+
 
 const app = express();
 
+app.use(helmet());
+app.use(
+    cors({
+      origin: '*',
+    })
+  );
+app.use(express.json());
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: false }));
 
 module.exports = app;
