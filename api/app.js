c5b1ac097084e1112a63bd03bdd5667e986f0b6a
loshjarson <jlarson129@gmail.com>
2023-05-16 12:36:30 -0400

    database connection and routing

--- api/app.js
+++ api/app.js
@@ -4,12 +4,21 @@ const logger = require('morgan');
 const helmet = require('helmet'); 
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
-const dotenv = require('dotenv');
-const config_result = dotenv.config();
+const dotenv = require('dotenv').config();
 
 
 const app = express();
 
+
+//Database Connection
+const mongoose = require("mongoose")
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('open', ()=> console.log("Connected to database"))
+
+
+//Middleware
 app.use(helmet());
 app.use(
     cors({
@@ -19,5 +28,12 @@ app.use(
 app.use(express.json());
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
+
+//Routes
+const usersRouter = require('./Routes/users')
+const recipesRouter = require('./Routes/recipes')
+
+app.use('/users', usersRouter)
+app.use('/recipes', recipesRouter)
 
 module.exports = app;
