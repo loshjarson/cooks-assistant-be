3cc3a35c177eeca16a521971a235151c28261e38
loshjarson <jlarson129@gmail.com>
2023-05-16 12:36:42 -0400

    route skeletons

---
+++ api/Routes/users.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+
+router.get('/', (req,res) => {
+    
+})
+
+router.get('/:id', (req,res) => {
+
+})
+
+router.post('/', (req,res) => {
+
+})
+
+router.patch('/', (req,res) => {
+
+})
+
+router.delete('/', (req,res) => {
+
+})
+
+
+
+module.exports = router
