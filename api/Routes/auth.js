2d04cbabbb2110da2882551ebed9615fbd935f2a
loshjarson <jlarson129@gmail.com>
2023-05-17 14:29:43 -0400

    login and register

--- api/Routes/auth.js
+++ api/Routes/auth.js
@@ -1,11 +1,54 @@
 const router = require("express").Router
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+
 const User = require("../../data/user")
 
-router.post("/register", (req,res) => {
+const makeToken = (user) => {
+    const payload = {
+        subject: user._id,
+        username: user.username,
+    }
+
+    const options = {
+        expiresIn: "1d",
+    }
     
+    return jwt.sign(payload, process.env.ACCESS_SECRET, options);
+}
+
+router.post("/register", async (req,res) => {
+    try {
+        const {username, password} = req.body;
+        const hash = bcrypt.hashSync(password)
+
+        const user = await User.create({username:username,password:hash})
+        if(user){
+            const token = makeToken(user);
+            res.status(201).json({
+                message: `Welcome, ${user.username}`,
+                token,
+            })
+        }
+    } catch (e) {
+        console.log(e.message)
+        res.status(401)
+    }
 })
 
-router.post("/login", (req,res) => {
+router.post("/login", async (req,res) => {
+    const {username, password} = req.body;
+
+    const user = await User.findOne({username})
     
+    if(user && bcrypt.compareSync(password,user.password)) {
+        const token = makeToken(savedUser);
+        res.status(200).json({
+            message: `${savedUser.username} is back!`,
+            token,
+            userID: savedUser.id
+        });
+    } else {
+        res.status(401).json({ message: "Please check credentials" });
+    }
 })
