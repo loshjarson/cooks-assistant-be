b383a765706ac91cba4aa9134b1cdffa2b56e30c
loshjarson <jlarson129@gmail.com>
2023-05-19 14:45:56 -0400

    basic routes to add, get, and update

--- api/Routes/recipes.js
+++ api/Routes/recipes.js
@@ -1,22 +1,42 @@
 const router = require('express').Router()
+const Recipe = require("../../data/recipe")
 
-router.get('/', (req,res) => {
+router.get('/:userId', async (req,res) => {
+    try {
+        const recipes = await Recipe.find({owner:req.params.userId})
+        res.status(201).json({message:"recipes gathered succesfully", recipes})
+    } catch (e) {
+        console.log(e.message)
+        res.status(400)
+    }
+})
+
+router.get('/:userId/:recipeId', (req,res) => {
     
 })
 
-router.get('/:id', (req,res) => {
-
+router.post('/', async (req,res) => {
+    try {
+        const recipe = await Recipe.create(req.body)
+        res.status(201).json({message:"recipe succesfully saved", recipe})
+    } catch (e) {
+        console.log(e.message)
+        res.status(401)
+    }
 })
 
-router.post('/', (req,res) => {
-
+router.patch('/:recipeId', async (req,res) => {
+    try {
+        const recipe = await Recipe.findByIdAndUpdate(req.params.recipeId,req.body,{new:true})
+        res.status(201).json({message:"recipe succesfully updated", recipe})
+    } catch (e) {
+        console.log(e.message)
+        res.status(401)
+    }
+    
 })
 
-router.patch('/', (req,res) => {
-
-})
-
-router.delete('/', (req,res) => {
+router.delete('/:recipeId', (req,res) => {
 
 })
 
