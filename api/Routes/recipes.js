0a14f70808050e4d3b3cdf81939babbe2db5f190
loshjarson <jlarson129@outlook.com>
2023-07-05 16:24:31 -0400

    production recipe list post endpoint

--- api/Routes/recipes.js
+++ api/Routes/recipes.js
@@ -99,6 +99,24 @@ router.post('/:recipeId/image', uploadImage.single('image'), (req, res) => {
     );
 })
 
+router.post('/:userId/list', async (req,res) => {
+    try {
+        const recipeList = JSON.parse(req.body)
+        const userId = req.params.userId
+
+        const list = new RecipeList({recipes:recipeList, owner:userId})
+
+        let savedList = await list.save() 
+        res.status(201).json({savedList})
+
+    } catch (e) {
+        console.log(e.message)
+        res.status(401)
+    }
+    
+})
+
+
 router.patch('/:recipeId', uploadImage.single('image'), async (req,res) => {
     try {
         const update = req.body;
