6177c9b80777e2eca100b8caf7f38b21f0e68e8b
loshjarson <jlarson129@outlook.com>
2023-07-10 17:31:35 -0400

    recipe list name

--- data/recipeList.js
+++ data/recipeList.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const recipeListSchema = new mongoose.Schema({
+    name: String,
     recipes: [{type: mongoose.Schema.Types.ObjectId, ref:'Recipe'}],
     owner: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
     updatedAt: { type: Date, default: () => Date.now() }
