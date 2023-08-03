0949592b27ca812bfae87201fda3489632c18db2
loshjarson <jlarson129@outlook.com>
2023-08-03 10:58:26 -0400

    full functionality of recipe list router

--- api/Routes/recipe-lists.js
+++ api/Routes/recipe-lists.js
@@ -20,10 +20,44 @@ router.post('/:userId', async (req,res) => {
 router.get('/:userId', async (req,res) => {
     try {
         const userId = req.params.userId
-        const recipeLists = RecipeList.find({owner:userId}).exec()
+        const recipeLists = await RecipeList.find({owner:userId}).exec()
 
         res.status(200).json({recipeLists})
 
+    } catch (e) {
+        console.log(e.message)
+        res.status(401)
+    }
+})
+
+router.put('/:recipeListId', async (req, res)=> {
+    try {
+        const listId = req.params.recipeListId
+        req.body.recipes = JSON.parse(req.body.recipes)
+
+        const updatedList = await RecipeList.findByIdAndUpdate(listId, req.body)
+
+        if(updatedList !== null){
+            res.status(201).json({updatedList})
+        } else {
+            res.status(204)
+        }
+    } catch (e) {
+        console.log(e.message)
+        res.status(401)
+    }
+})
+
+router.delete("/:recipeListId", async (req, res) => {
+    try {
+        const listId = req.params.recipeListId
+        const deletedList = await RecipeList.findByIdAndDelete(listId).exec()
+
+        if(deletedList != null){
+            res.status(201).json({message:"Recipe deleted",deletedList})
+        } else {
+            res.status(204).json({message:"No matching recipe found"})
+        }  
     } catch (e) {
         console.log(e.message)
         res.status(401)
