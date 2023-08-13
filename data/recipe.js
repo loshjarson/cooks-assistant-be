90e7e3f05c055f0efc04c58ee5e5280948ac36a9
loshjarson <jlarson129@outlook.com>
2023-08-13 19:30:22 -0400

    middleware to delete references on delete

--- data/recipe.js
+++ data/recipe.js
@@ -25,4 +25,26 @@ const recipeSchema = new mongoose.Schema({
     updatedAt: { type: Date, default: () => Date.now() }
 })
 
+// middleware to remove references from recipe lists
+recipeSchema.pre('delete', async function(next) {
+    try {
+        // 'this' refers to the recipe being deleted
+
+        // find all recipe lists that reference this recipe
+        const RecipeList = mongoose.model('RecipeList');
+        const lists = await RecipeList.find({ recipes: this._id });
+
+        // Loop through the lists and remove the reference to the deleted recipe
+        for (const list of lists) {
+            list.recipes.pull(this._id);
+            await list.save();
+        }
+
+        // Proceed with the recipe deletion
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
 module.exports = mongoose.model('Recipe', recipeSchema)
