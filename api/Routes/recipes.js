c001cc34227abdf1c6e23c0c20da8ab5ee2e2f8a
loshjarson <jlarson129@outlook.com>
2023-09-08 20:17:18 -0400

    test

--- api/Routes/recipes.js
+++ api/Routes/recipes.js
@@ -102,10 +102,12 @@ router.put('/:recipeId', uploadImage.single('image'), async (req,res) => {
         if(updatedRecipe.image){
             const recipeImage = await getFile(updatedRecipe.image.key)
             updatedRecipe.image = recipeImage
+            console.log("new",updatedRecipe)
             res.status(201).json(updatedRecipe)
         } else {
             const recipeImage = await getFile("3aa453485ddbbbbb3be4bc83d11ba3cb")
             updatedRecipe.image = recipeImage
+            console.log("def",updatedRecipe)
             res.status(201).json(updatedRecipe)
         }
         
