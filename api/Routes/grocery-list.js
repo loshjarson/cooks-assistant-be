0a363361b63eb79d4d8dd569bfa014ea71b2550a
loshjarson <jlarson129@outlook.com>
2023-09-08 22:33:06 -0400

    cleanup

--- api/Routes/grocery-list.js
+++ api/Routes/grocery-list.js
@@ -21,13 +21,13 @@ router.put("/update", async (req,res) => {
     try{
         const userId = new mongoose.Types.ObjectId(req.user)
         const updates = req.body;
-        console.log(updates)
+
         updates.recipes = JSON.parse(req.body.recipes)
-        console.log(updates.recipes)
+
         const promises = await Promise.all(updates.recipes.map(async recipe => {
             return await GroceryList.findOneAndUpdate({owner:userId, "groceries.recipe":recipe.recipe}, {$set: {"groceries.$.quantity":recipe.quantity}}, {new:true})
         }))
-        console.log(promises)
+
         const updatedList = await GroceryList.findOne({owner:userId}).lean()
         if(updatedList){
             res.status(201).json(updatedList)
